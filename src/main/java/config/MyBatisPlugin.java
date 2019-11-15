e7ac6d90892ccf3bea288cb1caa86d6287ce8c2a
yupan <yupan@yijiupi.cn>
2019-11-15 14:24:48 +0800

    mybatis-generator

---
+++ src/main/java/config/MyBatisPlugin.java
@@ -0,0 +1,61 @@
+package config;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.internal.util.StringUtility;
+
+import java.util.List;
+
+/**
+ * 自定义生成规则
+ */
+public class MyBatisPlugin extends PluginAdapter {
+
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        return true;
+    }
+
+    /**
+     * 字段注释
+     * @return
+     */
+    @Override
+    public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn,
+                                       IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        field.addJavaDocLine("/**");
+        String remarks = introspectedColumn.getRemarks();
+        if (StringUtility.stringHasValue(remarks)) {
+            String[] remarkLines = remarks.split(System.getProperty("line.separator"));
+            for (String remarkLine : remarkLines) {
+                field.addJavaDocLine(" * " + remarkLine);
+            }
+        }
+        field.addJavaDocLine(" */");
+        return true;
+    }
+
+    public boolean validate(List<String> list) {
+        return true;
+    }
+
+    @Override
+    public boolean clientGenerated(Interface interfaze, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        return true;
+    }
+
+    @Override
+    public boolean modelSetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        //不生成getter
+        return false;
+    }
+
+    @Override
+    public boolean modelGetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        //不生成setter
+        return false;
+    }
+
+}
